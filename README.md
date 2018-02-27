55351d1a55a56ac05ce7f9552e4053cddd6f707f
yangchao <yangchao@yun-kai.com>
2018-02-27 16:42:08 +0800

    修正i18n

--- README.md
+++ README.md
@@ -18,4 +18,7 @@
 * 使用json文件进行本地化配置，语言文件根目录为 **/lang**
 * 添加新增资源：yk
 * 完善ABP内部的本地化资源(Abp、AbpWeb和AbpZero)未的汉化部分
-* 支持i18n，统一配置到 ** src/YkAbp.Web.Host/lang**。i18n不支持在自定义模块中新增的语言资源。
+* 支持i18n，统一配置到 ** src/YkAbp.Web.Host/lang** 。i18n不支持在自定义模块中新增的语言资源。
+* 使用YkAbpUserConfiguration/GetAll替代原AbpUserConfiguration/GetAll接口（获取用户相关的系统配置数据）。
+
+>  **待优化**：由于[AbpUserConfigurationBuilder](https://github.com/aspnetboilerplate/aspnetboilerplate/blob/3337d1e2d8e8e6225ed5c28020e16cdc5562cd99/src/Abp.Web.Common/Web/Configuration/AbpUserConfigurationBuilder.cs)不易扩展（没有接口不能重新实现、属性全是私有的不能继承、GetAll不是virtual)，所以重新写了YkAbpUserConfigurationBuilder和YkAbpUserConfigurationController，不够灵活。
---
+++ src/YkAbp.Web.Core/Controllers/YkAbpUserConfigurationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YkAbp.Web.Core.Configuration;
+
+namespace YkAbp.Web.Core.Controllers
+{
+    public class YkAbpUserConfigurationController: YkAbpControllerBase
+    {
+        private readonly YkAbpUserConfigurationBuilder _abpUserConfigurationBuilder;
+
+        public YkAbpUserConfigurationController(YkAbpUserConfigurationBuilder abpUserConfigurationBuilder)
+        {
+            _abpUserConfigurationBuilder = abpUserConfigurationBuilder;
+        }
+
+        public async Task<JsonResult> GetAll()
+        {
+            var userConfig = await _abpUserConfigurationBuilder.GetAll();
+            return Json(userConfig);
+        }
+    }
+}
